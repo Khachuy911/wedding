47ddd21d7178314073b69f046841a8ef3ca4d422
hoangkz <hoangyl245@gmail.com>
2026-03-10 21:03:34 +0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,15 +3,16 @@ import { Metadata } from "next"
 import { ToastContainer } from "react-toastify"
 import "./globals.css"
 export const metadata: Metadata = {
-  metadataBase: new URL(process.env.NEXT_PUBLIC_BASE_URL || 'https://your-wedding.vercel.app'),
+  metadataBase: new URL('https://huylien-wedding.vercel.app'),
   title: "Thiệp cưới online",
   description: "Trân trọng kính mời bạn đến dự lễ cưới của chúng tôi",
   openGraph: {
     title: "Thiệp cưới online",
+    url: 'https://huylien-wedding.vercel.app',
     description: "Trân trọng kính mời bạn đến dự lễ cưới của chúng tôi",
     images: [
       {
-        url: "/layout/couple.png",
+        url: "/1.jpg",
         width: 1200,
         height: 630,
         alt: "Ảnh cưới",
@@ -24,7 +25,7 @@ export const metadata: Metadata = {
     card: "summary_large_image",
     title: "Thiệp cưới online",
     description: "Trân trọng kính mời bạn đến dự lễ cưới của chúng tôi",
-    images: ["/layout/couple.png"],
+    images: ["/1.jpg"],
   },
 }
 
