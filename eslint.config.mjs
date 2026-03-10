9a3e0db6526b8da0da1d114aa7afa0e8fa43859b
hoangkz <hoangyl245@gmail.com>
2026-03-11 03:40:18 +0700

    update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,6 +15,7 @@ const eslintConfig = [
   {
     ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"],
     rules: {
+      "@typescript-eslint/no-explicit-any": "off",
       "import/no-unresolved": "error",
     },
   },
